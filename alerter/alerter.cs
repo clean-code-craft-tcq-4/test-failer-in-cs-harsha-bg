b68029b0ce170a88ecedc345b70e30800296efd6
B G Harsha <hbg3kor@bosch.com>
2022-06-22 23:46:54 +0530

    CodeCheckIn: test-failers

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,19 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
 
-namespace AlerterSpace {
-    class Alerter {
+namespace AlerterSpace
+{
+    class Alerter
+    {
         static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
+        static int networkAlertStub(float celcius)
+        {
             Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
+            if (celcius > 200)
+                return 500;
             return 200;
         }
-        static void alertInCelcius(float farenheit) {
+        static void alertInCelcius(float farenheit)
+        {
             float celcius = (farenheit - 32) * 5 / 9;
             int returnCode = networkAlertStub(celcius);
-            if (returnCode != 200) {
+            if (returnCode != 200)
+            {
                 // non-ok response is not an error! Issues happen in life!
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
@@ -21,11 +29,35 @@ namespace AlerterSpace {
                 alertFailureCount += 0;
             }
         }
-        static void Main(string[] args) {
-            alertInCelcius(400.5f);
-            alertInCelcius(303.6f);
+        static void Main(string[] args)
+        {
+            AlertTestMethod();
+            NonAlertTestMethod();
+
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Console.WriteLine("All is well (maybe!)\n");
+        }
+
+        private static void NonAlertTestMethod()
+        {
+            float farenheit = 303.6f;
+            alertInCelcius(farenheit);
+            AssertFailureStatus(false);
+        }
+
+        private static void AlertTestMethod()
+        {
+            float farenheit = 400.5f;
+            alertInCelcius(farenheit);
+            AssertFailureStatus(true);
+        }
+
+        public static void AssertFailureStatus(bool IsFailureExpected)
+        {
+            if (alertFailureCount == 0 && !IsFailureExpected)
+                Debug.Assert(true);
+            else
+                Debug.Assert(false);
         }
     }
 }
